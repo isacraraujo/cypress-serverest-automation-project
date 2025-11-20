44a190f33424a32a9fd438b29de750c373999669
isacraraujo <1s4c.3d@gmail.com>
2025-11-20 09:18:32 -0300

    build: configuração de relatórios e variáveis de ambiente para o projeto

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,35 @@
 const { defineConfig } = require("cypress");
+require('dotenv').config();
 
 module.exports = defineConfig({
+  env: {
+    url_application: process.env.URL_APPLICATION,
+    url_api:         process.env.URL_API,
+    user_auth:       process.env.USER_AUTH,
+    password_auth:   process.env.PASSWORD_AUTH,
+    user_admin:      process.env.USER_ADMIN,
+    password_admin:  process.env.PASSWORD_ADMIN
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+      // exportar o módulo de relatórios do Mochawesome
+      require('cypress-mochawesome-reporter/plugin')(on);
+      // configurações de responsividade mínima para desktop
+      config.desktopViewWidth  = 1368;
+      config.desktopViewHeight = 768;
+      return config;
+    }
   },
+  // configurações personalizadas do relatório Mochawesome
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir:           'cypress/reports/',
+    reportFilename:      'report',
+    reportTitle:         'Testes Automatizados com Cypress',
+    reportPageTitle:     'Testes Automatizados com Cypress',
+    charts:              true,
+    embeddedScreenshots: true,
+    inlineAssets:        true,
+    saveAllAttempts:     false,
+  }
 });
